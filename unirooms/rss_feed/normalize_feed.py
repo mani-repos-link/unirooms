34c749b037cefbffc52f12518c0e3da7614d73aa
Phoenix404 <manpreet2681995@gmail.com>
2019-04-24 14:46:47 +0200

    Fix __get_lecture_type method

--- unirooms/rss_feed/normalize_feed.py
+++ unirooms/rss_feed/normalize_feed.py
@@ -4,12 +4,7 @@ import json
 class NormalizeFeed:
     def __init__(self, file):
         self.filename = file
-        self.lecture_type = {
-            'lect': 'LECT',
-            'lab': 'LAB',
-            'unknown': 'UNKNOWN'
-        }
-        pass
+        self.lecture_type = {'lect': 'LECT', 'lab': 'LAB', 'unknown': 'UNKNOWN'}
 
     def normalize(self):
         self.data = []
@@ -17,20 +12,21 @@ class NormalizeFeed:
             self._feed = json.load(f)
 
         for obj in self._feed:
-            print(obj)
-            print(self.__get_lecture_type(obj.title))
-            break
+            lect_type = self.__get_lecture_type(obj['title'])
             normlized_object = {
                 "date": obj.date,
                 "start": obj.time.split[' '][0],
                 "end": obj.time.split[' '][1],
-                "title": obj.title.split[' '][1],
+                "title": (obj.title.split[' '][1]).replace(lect_type, ''),
+                "type": lect_type,
             }
             self.data.append(normlized_object)
 
     def __get_lecture_type(self, title):
         if len(title) < 8:
             return self.lecture_type['unknown']
-        last_chr = title[-1:8]
-        print(last_chr)
-        return last_chr
+        last_chars = (title[-5:]).rstrip().lstrip()
+        if last_chars not in self.lecture_type:
+            last_chars = self.lecture_type['unknown']
+        return last_chars
+
